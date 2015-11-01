94904271de26c42c4771e58ee96fc7b4f402cece
bawg <james.minneman@gmail.com>
2015-11-01 23:38:26 +0000

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,15 +1,4 @@
 import json
-from json import load
-from urllib2 import urlopen
-from pprint import pprint
-import pycassa
-from pycassa.pool import ConnectionPool
-from pycassa.columnfamily import ColumnFamily
-from pycassa.index import *
 
-pool = pycassa.ConnectionPool('hr')
-crime = pycassa.ColumnFamily(pool, 'crime')
 
-##Pull all of the data and convert to JSON 
-data = json.dumps([columns for key, columns in crime.get_range()])
-print data
+print 'hello'
