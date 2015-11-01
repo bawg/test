49c2122d094f9e36f99374fe460705bd9d5f6e4d
bawg <james.minneman@gmail.com>
2015-11-01 23:44:56 +0000

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -1,4 +1,21 @@
+#!/usr/bin/python 
+
+# Import modules for CGI handling 
+import cgi, cgitb 
+import urllib2 
 import json
 
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
 
-print 'hello'
+#download data from request parameter 'url' 
+print "Content-type:text/javascript\r\n\r\n" 
+url = form.getvalue("url") 
+callback = form.getvalue("callback")
+req = urllib2.Request(url) 
+response = urllib2.urlopen(req) 
+try:
+    data = response.read() 
+    print callback + "(" + json.dumps(data)+ ")"
+finally:
+    response.close()
